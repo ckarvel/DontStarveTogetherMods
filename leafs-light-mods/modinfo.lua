6947255d56b50004974779c7470bc4bcda13778a
Carina Vargas <car.vargasv@gmail.com>
2021-04-09 22:34:19 -0400

    added turning off miners hat

---
+++ leafs-light-mods/modinfo.lua
@@ -0,0 +1,18 @@
+name = "Light Source Mods"
+description = [[
+    - Can turn off miner's hat
+    - Light amulet
+]]
+author = "amoryleaf"
+version = "1.0.0"
+
+forumthread = ""
+
+api_version = 10
+
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+dst_compatible = true
+
+all_clients_require_mod = true
+clients_only_mod = false
